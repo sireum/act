94542f505e492e57d87ecfe6efa037c0fc79cac1
Robby <robby@santoslab.org>
2021-06-14 10:03:52 -0500

    Uniformized copyright years.

--- jvm/src/main/scala/org/sireum/hamr/act/cli.scala
+++ jvm/src/main/scala/org/sireum/hamr/act/cli.scala
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2018, Jason Belt, Kansas State University
+ Copyright (c) 2017-2021, Jason Belt, Kansas State University
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
