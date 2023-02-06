97e86f122482ceb0c780f8c75f19869c21e41532
Robby <robby@santoslab.org>
2023-02-06 16:48:31 -0600

    Updated trafo.

--- jvm/src/main/scala/org/sireum/hamr/act/ast/regen.sh
+++ jvm/src/main/scala/org/sireum/hamr/act/ast/regen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 $SIREUM_HOME/bin/sireum tools sergen -l $SIREUM_HOME/license.txt -m json,msgpack ActAst.scala
-$SIREUM_HOME/bin/sireum tools transgen -l $SIREUM_HOME/license.txt -m immutable,mutable ActAst.scala
+$SIREUM_HOME/bin/sireum tools trafo -l $SIREUM_HOME/license.txt -m immutable,mutable ActAst.scala
